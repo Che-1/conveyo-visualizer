5be60e34bfbdc8585d542ec0a678c1fda8e1f09d
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-06-23 02:16:41 +0900

    Upgrade styles

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+
+const stagingList = 'abcdefghijklmnop'.split('');
 
 
 const EVENT_DIRECTION = {
-  order_submitted: 'Right',
-  order_send: 'Right',
-  conveyo_order_send: 'Right',
-  conveyo_opa_order_send: 'Right',
-  conveyo_sms_order_send: 'Right',
-  relayo_order_send: 'Right',
-  conveyo_alarm_invoke: 'Left',
-  yogiyo_dowant_opa_order_callback_send: 'Left',
-  yogiyo_dowant_relayo_order_callback_send: 'Left',
-  yogiyo_dowant_sms_order_callback_send: 'Left',
-  conveyo_order_callback_send: 'Left',
-  conveyo_order_changed: 'Right',
-  orderyo_alarm_receive: 'Left',
-  orderyo_rider_status_callback: 'Left',
-  conveyo_opa_order_cancel: 'Right',
-  orderyo_order_timeout: 'Left',
-  order_status_updated: 'Left'
+  order_submitted: "Right",
+  order_send: "Right",
+  conveyo_order_send: "Right",
+  conveyo_opa_order_send: "Right",
+  conveyo_sms_order_send: "Right",
+  relayo_order_send: "Right",
+  conveyo_alarm_invoke: "Left",
+  yogiyo_dowant_opa_order_callback_send: "Left",
+  yogiyo_dowant_relayo_order_callback_send: "Left",
+  yogiyo_dowant_sms_order_callback_send: "Left",
+  conveyo_order_callback_send: "Left",
+  conveyo_order_changed: "Right",
+  orderyo_alarm_receive: "Left",
+  orderyo_rider_status_callback: "Left",
+  conveyo_opa_order_cancel: "Right",
+  orderyo_order_timeout: "Left",
+  order_status_updated: "Left"
 }
 
 const ORIGIN_CONVEYO_EVENTS = [
-  'order_submitted',
-  'conveyo_order_send',
-  'order_send',
-  'yogiyo_dowant_opa_order_callback_send',
-  'yogiyo_dowant_relayo_order_callback_send',
-  'yogiyo_dowant_sms_order_callback_send',
-  'orderyo_alarm_receive',
-  'conveyo_opa_order_cancel',
-  'orderyo_order_timeout',
-  'order_status_updated'
+  "order_submitted",
+  "conveyo_order_send",
+  "order_send",
+  "yogiyo_dowant_opa_order_callback_send",
+  "yogiyo_dowant_relayo_order_callback_send",
+  "yogiyo_dowant_sms_order_callback_send",
+  "orderyo_alarm_receive",
+  "conveyo_opa_order_cancel",
+  "orderyo_order_timeout",
+  "order_status_updated"
 ]
 
 const CONVEYO_TARGET_EVENTS = [
-  'conveyo_opa_order_send',
-  'relayo_order_send',
-  'conveyo_sms_order_send',
-  'conveyo_alarm_invoke',
-  'conveyo_order_callback_send',
-  'orderyo_rider_status_callback',
-  'conveyo_order_changed'
+  "conveyo_opa_order_send",
+  "relayo_order_send",
+  "conveyo_sms_order_send",
+  "conveyo_alarm_invoke",
+  "conveyo_order_callback_send",
+  "orderyo_rider_status_callback",
+  "conveyo_order_changed"
 ]
 
 const STATUS_UPDATE_EVENTS = [
-  'yogiyo_dowant_opa_order_callback_send',
-  'yogiyo_dowant_relayo_order_callback_send',
-  'yogiyo_dowant_sms_order_callback_send',
-  'order_status_updated'
+  "yogiyo_dowant_opa_order_callback_send",
+  "yogiyo_dowant_relayo_order_callback_send",
+  "yogiyo_dowant_sms_order_callback_send",
+  "order_status_updated"
 ]
 
 function getTitle(event) {
   let title
   if (STATUS_UPDATE_EVENTS.includes(event.meta.event)) {
     title = `${event.meta.event}:\n${event.data ? event.data.event : JSON.parse(event.response).event}`
-  } else if (event.meta.event === 'conveyo_order_callback_send') {
+  } else if (event.meta.event === "conveyo_order_callback_send") {
     title = `${event.meta.event}:\n${event.transmission_data ? event.transmission_data.data.event : event.target_response.status}`
-  } else if (event.meta.event === 'orderyo_rider_status_callback') {
+  } else if (event.meta.event === "orderyo_rider_status_callback") {
     title = event.target_response.event
   } else {
     title =  event.meta.event
@@ -68,19 +71,20 @@ function getTitle(event) {
 }
 
 
-function Arrow({ event, handleClick }) {
+function Arrow({ selected, event, handleClick }) {
   const eventName = event.meta.event
   const direction = EVENT_DIRECTION[eventName]
   const className = "arrowHead arrow" + direction
   return (
     <div className="arrow">
-      <div className="arrowTitle" onClick={handleClick}>{getTitle(event)}</div>
+      <div className={`arrowTitle ${selected ? "selected" : null}`} onClick={handleClick}>{getTitle(event)}</div>
       <div className={className}/>
     </div>
   )
 }
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [events, setEvents] = useState()
   const [orderSendEvent, setOrderSendEvent] = useState();
 
@@ -89,30 +93,64 @@ function App() {
 
   const [currentEvent, setCurrentEvent] = useState();
   const [showEventLog, setShowEventLog] = useState(false);
+  const [logPosition, setLogPosition] = useState();
+  const [selectedEventIndex, setSelectedEventIndex] = useState();
 
-  const [env, setEnv] = useState('hubyo');
+  const [env, setEnv] = useState("hubyo");
   const [trackingId, setTrackingId] = useState(647818940);
-  const [platform, setPlatform] = useState('YGY-DOWANT');
+  const [platform, setPlatform] = useState("YGY-DOWANT");
+
+  const clearState = () => {
+    setEvents([]);
+    setShowEventLog(false);
+    setCurrentEvent(undefined);
+    setSelectedEventIndex(undefined);
+  }
+
+  const closeModal = () => {
+    setCurrentEvent(undefined); 
+    setShowEventLog(false);
+    setLogPosition(undefined);
+  }
+
+  const handleEnter = (e) => {
+    if (e.code === "Enter") {
+      getHubyoData()
+    }
+  }
 
   const getHubyoData = () => {
+    clearState();
+    setIsLoading(true)
+
     const request = new XMLHttpRequest()
-    request.open('GET', `https://${env}.yogiyo.co.kr/list?platform=${platform}&entity=order&tracking_id=${trackingId}`)
+    request.open("GET", `https://${env}.yogiyo.co.kr/list?platform=${platform}&entity=order&tracking_id=${trackingId}`)
     request.onreadystatechange = () => {
       if (request.readyState === XMLHttpRequest.DONE) {
         const eventList = JSON.parse(request.response)
-        setEvents(eventList.map((value, index) => {value['eventIndex'] = index + 1; return value}));
-        setShowEventLog(false);
-        setCurrentEvent(undefined);
-
+        setEvents(eventList.map((value, index) => {value["eventIndex"] = index + 1; return value}));
+        setIsLoading(false);
       }
     }
     request.send()
   }
 
+  const selectPlatform = (platform) => {
+    if (platform.includes("staging")) {
+      setEnv("staging-hubyo")
+    } else {
+      setEnv("hubyo")
+    }
+    setPlatform(platform)
+  }
+
   useEffect(() => {
-    setInterval(() => {
-      // getHubyoData()
-    }, 1000)
+    document.addEventListener("keydown", (e) => {
+      console.log(e)
+      if (e.code === "Escape") {
+        closeModal()
+      }
+    })
   }, [])
 
   useEffect(() => {
@@ -133,7 +171,7 @@ function App() {
   return (
     <div className="App">
       <div className="rootContainer">
-        <div>
+        <div  className="header">
           <div className="hubyoUrlDisplay">
             https://<span className="hubyoUrlBold">{env}</span>
             {`.yogiyo.co.kr/list?platform=`}
@@ -141,53 +179,83 @@ function App() {
             {`&entity=order&tracking_id=`}
             <span className="hubyoUrlBold">{trackingId}</span>
           </div>
-          <div className="inputWrapper">
-            <label>Platform</label>
-            <input title="platform" onChange={(e) => setPlatform(e.target.value)} value={platform} />
-            <label>Tracking ID</label>
-            <input title="trackingId" onChange={(e) => setTrackingId(e.target.value)} value={trackingId} />
-            <label>Env</label>
-            <input title="env" onChange={(e) => setEnv(e.target.value)} value={env} />
-            <button onClick={getHubyoData}>Send Request</button>
-            <button onClick={() => currentEvent ? setShowEventLog(!showEventLog) : null}>Show Message Log</button>
+          <div className="inputContainer">
+            <div className="inputLeftSection">
+              <div className="inputWrapper">
+                <label>Platform</label>
+                <select onKeyDown={handleEnter} className="inputStyle" name="platform" placeholder="Platform" onChange={(e) => selectPlatform(e.target.value)} value={platform}>
+                  <optgroup label="Staging">
+                    {stagingList.map((value, index) => {
+                      return <option key={index} value={`YGY-staging-${value}`}>YGY-staging-{value}</option>
+                    })}
+                  </optgroup>
+                  <optgroup label="Production">
+                    <option value="YGY">YGY</option>
+                    <option value="YGY-DOWANT">YGY-DOWANT</option>
+                  </optgroup>
+                </select>
+              </div>
+              <div className="inputWrapper">
+                <label>Tracking ID</label>
+                <input onKeyDown={handleEnter} style={{width: "105px"}} className="inputStyle" title="trackingId" onChange={(e) => setTrackingId(e.target.value)} value={trackingId} />
+              </div>
+            </div>
+            <div className="inputRightSection">
+              <div className={isLoading ? "spinner spinning" : "spinner"}/>
+              <button className="requestButton" onClick={getHubyoData}>Send Request</button>
+            </div>
           </div>
         </div>
         {events ? 
           <div className="componentsDisplay"> 
-            <div className="origin">
-              Origin {`${orderSendEvent ? `(${orderSendEvent.meta.publisher})` : ''}`}
+            <div className="component">
+              Origin {`${orderSendEvent ? `(${orderSendEvent.meta.publisher})` : ""}`}
             </div>
 
             <div className="arrowContainer">
               {originConveyoEvents.map((value, index) => {
-                return <Arrow key={index} event={value} handleClick={() => {setCurrentEvent(value); setShowEventLog(true)}}/>
+                return <Arrow key={index} selected={index === selectedEventIndex && logPosition === "R"} event={value} handleClick={() => {
+                  setCurrentEvent(value); 
+                  setShowEventLog(true);
+                  setLogPosition("R");
+                  setSelectedEventIndex(index);
+                }}/>
               })}
             </div>
 
-            <div className="origin">
+            <div className="component">
               Conveyo
             </div>
 
             <div className="arrowContainer">
               {conveyoTargetEvents.map((value, index) => {
-                return <Arrow key={index} event={value} handleClick={() => {setCurrentEvent(value); setShowEventLog(true)}}/>
+                return <Arrow key={index} selected={index === selectedEventIndex && logPosition === "L"} event={value} handleClick={() => {
+                  setCurrentEvent(value); 
+                  setShowEventLog(true);
+                  setLogPosition("L");
+                  setSelectedEventIndex(index);
+                }}/>
               })}
             </div>
 
-            <div className="origin">
+            <div className="component">
               Targets
             </div>
           </div>
         :null}
-        {showEventLog? 
-        <div className="eventViewContainer">
-          <pre className="codePreview">
-            {JSON.stringify(currentEvent, null, 2)}
-          </pre>
+        {showEventLog?
+        <div className={`eventViewContainer ${logPosition === "L" ? "onLeft" : "onRight"}`}>
+          <button 
+          className="closeButton" 
+          onClick={closeModal}>Close
+          </button>
+          <div className="previewWrapper">
+            <pre className="codePreview">
+              {JSON.stringify(currentEvent, null, 2)}
+            </pre>
+          </div>
         </div> : null
         }
-        
-
       </div>
     </div>
   );
