d8f220f874044654287932be063368265ca63e3f
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-01 11:39:17 +0900

    Fix order status updated event parsing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-
-const stagingList = 'abcdefghijklmnop'.split('');
-
+const stagingList = "abcdefghijklmnop".split("");
 
 const EVENT_DIRECTION = {
   order_submitted: "Right",
@@ -24,8 +22,8 @@ const EVENT_DIRECTION = {
   orderyo_order_timeout: "Left",
   order_status_updated: "Left",
   orderyo_backend_process: "Left",
-  conveyo_opa_timeout_alarm_receive: "Left"
-}
+  conveyo_opa_timeout_alarm_receive: "Left",
+};
 
 const ORIGIN_CONVEYO_EVENTS = [
   "order_submitted",
@@ -38,8 +36,8 @@ const ORIGIN_CONVEYO_EVENTS = [
   "conveyo_opa_order_cancel",
   "orderyo_order_timeout",
   "order_status_updated",
-  "conveyo_opa_timeout_alarm_receive"
-]
+  "conveyo_opa_timeout_alarm_receive",
+];
 
 const CONVEYO_TARGET_EVENTS = [
   "conveyo_opa_order_send",
@@ -48,46 +46,55 @@ const CONVEYO_TARGET_EVENTS = [
   "conveyo_alarm_invoke",
   "conveyo_order_callback_send",
   "orderyo_rider_status_callback",
-  "conveyo_order_changed"
-]
+  "conveyo_order_changed",
+];
 
 const STATUS_UPDATE_EVENTS = [
   "yogiyo_dowant_opa_order_callback_send",
   "yogiyo_dowant_relayo_order_callback_send",
   "yogiyo_dowant_sms_order_callback_send",
-  "order_status_updated"
-]
+  "order_status_updated",
+];
 
 function getTitle(event) {
-  let title
+  let title;
   if (STATUS_UPDATE_EVENTS.includes(event.meta.event)) {
-    title = `${event.meta.event}:\n${event.data ? event.data.event : JSON.parse(event.response).event}`
+    if (event.event_type) {
+      title = `${event.meta.event}: ${event.event_type}`;
+    } else if (event.status) {
+      title = `${event.meta.event}: ${event.status}`;
+    } else {
+      title = `${event.meta.event}:\n${event.data ? event.data.event : JSON.parse(event.response).event}`;
+    }
   } else if (event.meta.event === "conveyo_order_callback_send") {
-    title = `${event.meta.event}:\n${event.transmission_data ? event.transmission_data.data.event : event.target_response.status}`
+    title = `${event.meta.event}:\n${
+      event.transmission_data ? event.transmission_data.data.event : event.target_response.status
+    }`;
   } else if (event.meta.event === "orderyo_rider_status_callback") {
-    title = event.target_response.event
+    title = event.target_response.event;
   } else if (event.meta.event === "orderyo_backend_process" && event.type === "transmission") {
-    title = event.message
+    title = event.message;
   } else if (event.meta.event === "orderyo_backend_process" && event.type === "claim_refund") {
-    title = `${event.type}: ${event.status}`
+    title = `${event.type}: ${event.status}`;
   } else {
-    title =  event.meta.event
+    title = event.meta.event;
   }
 
-  return `${event.eventIndex}: ${title}`
+  return `${event.eventIndex}: ${title}`;
 }
 
-
 function Arrow({ selected, event, handleClick }) {
-  const eventName = event.meta.event
-  const direction = EVENT_DIRECTION[eventName]
-  const className = "arrowHead arrow" + direction
+  const eventName = event.meta.event;
+  const direction = EVENT_DIRECTION[eventName];
+  const className = "arrowHead arrow" + direction;
   return (
     <div className="arrow">
-      <div className={`arrowTitle ${selected ? "selected" : null}`} onClick={handleClick}>{getTitle(event)}</div>
-      <div className={className}/>
+      <div className={`arrowTitle ${selected ? "selected" : null}`} onClick={handleClick}>
+        {getTitle(event)}
+      </div>
+      <div className={className} />
     </div>
-  )
+  );
 }
 
 function AlimTalk({ selected, event, handleClick }) {
@@ -95,12 +102,12 @@ function AlimTalk({ selected, event, handleClick }) {
     <div className={`alimTalkProcess ${selected ? "selected" : null}`} onClick={handleClick}>
       {event.eventIndex}: {event.meta.event} {event.type}
     </div>
-  )
+  );
 }
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
-  const [events, setEvents] = useState()
+  const [events, setEvents] = useState();
   const [orderSendEvent, setOrderSendEvent] = useState();
 
   const [originConveyoEvents, setOriginConveyoEvents] = useState([]);
@@ -121,93 +128,97 @@ function App() {
     setShowEventLog(false);
     setCurrentEvent(undefined);
     setSelectedEventIndex(undefined);
-  }
+  };
 
   const closeModal = () => {
-    setCurrentEvent(undefined); 
+    setCurrentEvent(undefined);
     setShowEventLog(false);
     setLogPosition(undefined);
     setSelectedEventIndex(undefined);
-  }
+  };
 
   const handleEnter = (e) => {
     if (e.code === "Enter") {
-      getHubyoData()
+      getHubyoData();
     }
-  }
+  };
 
   const getHubyoData = () => {
     clearState();
-    setIsLoading(true)
+    setIsLoading(true);
 
-    const request = new XMLHttpRequest()
-    request.open("GET", `https://${env}.yogiyo.co.kr/list?platform=${platform}&entity=order&tracking_id=${trackingId}`)
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://${env}.yogiyo.co.kr/list?platform=${platform}&entity=order&tracking_id=${trackingId}`);
     request.onreadystatechange = () => {
       if (request.readyState === XMLHttpRequest.DONE) {
-        const eventList = JSON.parse(request.response)
-        setEvents(eventList.map((value, index) => {value["eventIndex"] = index + 1; return value}));
+        const eventList = JSON.parse(request.response);
+        setEvents(
+          eventList.map((value, index) => {
+            value["eventIndex"] = index + 1;
+            return value;
+          })
+        );
         setIsLoading(false);
       }
-    }
-    request.send()
-  }
+    };
+    request.send();
+  };
 
   const selectPlatform = (platform) => {
     if (platform.includes("staging")) {
-      setEnv("staging-hubyo")
+      setEnv("staging-hubyo");
     } else {
-      setEnv("hubyo")
+      setEnv("hubyo");
     }
-    setPlatform(platform)
-  }
+    setPlatform(platform);
+  };
 
   const isTransmissionBackendProcess = (message) => {
-    return message.meta.event === "orderyo_backend_process" && ["transmission", "claim_refund"].includes(message.type)
-  }
+    return message.meta.event === "orderyo_backend_process" && ["transmission", "claim_refund"].includes(message.type);
+  };
 
   const isAlimTalkBackendProcess = (message) => {
-    return message.meta.event === "orderyo_backend_process" && message.type === "alim_talk"
-  }
+    return message.meta.event === "orderyo_backend_process" && message.type === "alim_talk";
+  };
 
   const handleEventClick = (value, index, position) => {
-    setCurrentEvent(value); 
+    setCurrentEvent(value);
     setShowEventLog(true);
     setLogPosition(position);
     setSelectedEventIndex(value.eventIndex);
-  }
+  };
 
   useEffect(() => {
     document.addEventListener("keydown", (e) => {
-      console.log(e)
       if (e.code === "Escape") {
-        closeModal()
+        closeModal();
       }
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
     if (events) {
-      const originConveyoEvents = events.filter((value) => ORIGIN_CONVEYO_EVENTS.includes(value.meta.event) || isTransmissionBackendProcess(value));
-      setOriginConveyoEvents(originConveyoEvents)
-      setOrderSendEvent(originConveyoEvents[0])
-      setConveyoTargetEvents(
-        events.filter((value) => CONVEYO_TARGET_EVENTS.includes(value.meta.event))
-      )
+      const originConveyoEvents = events.filter(
+        (value) => ORIGIN_CONVEYO_EVENTS.includes(value.meta.event) || isTransmissionBackendProcess(value)
+      );
+      setOriginConveyoEvents(originConveyoEvents);
+      setOrderSendEvent(originConveyoEvents[0]);
+      setConveyoTargetEvents(events.filter((value) => CONVEYO_TARGET_EVENTS.includes(value.meta.event)));
       setOrderyoBackendProcessEvents(
         events.filter((value) => value.meta.event === "orderyo_backend_process" && isAlimTalkBackendProcess(value))
-      )
+      );
     } else {
       setOrderyoBackendProcessEvents([]);
       setOriginConveyoEvents([]);
       setConveyoTargetEvents([]);
       setOrderSendEvent(undefined);
     }
-  }, [events])
+  }, [events]);
 
   return (
     <div className="App">
       <div className="rootContainer">
-        <div  className="header">
+        <div className="header">
           <div className="hubyoUrlDisplay">
             https://<span className="hubyoUrlBold">{env}</span>
             {`.yogiyo.co.kr/list?platform=`}
@@ -219,10 +230,21 @@ function App() {
             <div className="inputLeftSection">
               <div className="inputWrapper">
                 <label>Platform</label>
-                <select onKeyDown={handleEnter} className="inputStyle" name="platform" placeholder="Platform" onChange={(e) => selectPlatform(e.target.value)} value={platform}>
+                <select
+                  onKeyDown={handleEnter}
+                  className="inputStyle"
+                  name="platform"
+                  placeholder="Platform"
+                  onChange={(e) => selectPlatform(e.target.value)}
+                  value={platform}
+                >
                   <optgroup label="Staging">
                     {stagingList.map((value, index) => {
-                      return <option key={index} value={`YGY-staging-${value}`}>YGY-staging-{value}</option>
+                      return (
+                        <option key={index} value={`YGY-staging-${value}`}>
+                          YGY-staging-{value}
+                        </option>
+                      );
                     })}
                   </optgroup>
                   <optgroup label="Production">
@@ -233,63 +255,83 @@ function App() {
               </div>
               <div className="inputWrapper">
                 <label>Tracking ID</label>
-                <input onKeyDown={handleEnter} style={{width: "105px"}} className="inputStyle" title="trackingId" onChange={(e) => setTrackingId(e.target.value)} value={trackingId} />
+                <input
+                  onKeyDown={handleEnter}
+                  style={{ width: "105px" }}
+                  className="inputStyle"
+                  title="trackingId"
+                  onChange={(e) => setTrackingId(e.target.value)}
+                  value={trackingId}
+                />
               </div>
             </div>
             <div className="inputRightSection">
-              <div className={isLoading ? "spinner spinning" : "spinner"}/>
-              <button className="requestButton" onClick={getHubyoData}>Send Request</button>
+              <div className={isLoading ? "spinner spinning" : "spinner"} />
+              <button className="requestButton" onClick={getHubyoData}>
+                Send Request
+              </button>
             </div>
           </div>
         </div>
-        {events ? 
-          <div className="componentsDisplay"> 
-            {orderSendEvent && orderSendEvent.meta.publisher === "orderyo" ? 
+        {events ? (
+          <div className="componentsDisplay">
+            {orderSendEvent && orderSendEvent.meta.publisher === "orderyo" ? (
               <div className="orderyoBackendProcessContainer">
                 <div className="alimTalkTitle">Alim Talk</div>
                 {orderyoBackendProcessEvents.map((value, index) => {
-                    return <AlimTalk event={value} selected={value.eventIndex === selectedEventIndex} handleClick={() => handleEventClick(value, index, "R")}/>
-                  })}
-              </div> : null
-            }
-            <div className="component">
-              Origin {`${orderSendEvent ? `(${orderSendEvent.meta.publisher})` : ""}`}
-            </div>
+                  return (
+                    <AlimTalk
+                      event={value}
+                      selected={value.eventIndex === selectedEventIndex}
+                      handleClick={() => handleEventClick(value, index, "R")}
+                    />
+                  );
+                })}
+              </div>
+            ) : null}
+            <div className="component">Origin {`${orderSendEvent ? `(${orderSendEvent.meta.publisher})` : ""}`}</div>
 
             <div className="arrowContainer">
               {originConveyoEvents.map((value, index) => {
-                return <Arrow key={index} selected={value.eventIndex === selectedEventIndex} event={value} handleClick={() => handleEventClick(value, index, "R")}/>
+                return (
+                  <Arrow
+                    key={index}
+                    selected={value.eventIndex === selectedEventIndex}
+                    event={value}
+                    handleClick={() => handleEventClick(value, index, "R")}
+                  />
+                );
               })}
             </div>
 
-            <div className="component">
-              Conveyo
-            </div>
+            <div className="component">Conveyo</div>
 
             <div className="arrowContainer">
               {conveyoTargetEvents.map((value, index) => {
-                return <Arrow key={index} selected={value.eventIndex === selectedEventIndex} event={value} handleClick={() => handleEventClick(value, index, "L")}/>
+                return (
+                  <Arrow
+                    key={index}
+                    selected={value.eventIndex === selectedEventIndex}
+                    event={value}
+                    handleClick={() => handleEventClick(value, index, "L")}
+                  />
+                );
               })}
             </div>
 
-            <div className="component">
-              Targets
+            <div className="component">Targets</div>
+          </div>
+        ) : null}
+        {showEventLog ? (
+          <div className={`eventViewContainer ${logPosition === "L" ? "onLeft" : "onRight"}`}>
+            <button className="closeButton" onClick={closeModal}>
+              Close
+            </button>
+            <div className="previewWrapper">
+              <pre className="codePreview">{JSON.stringify(currentEvent, null, 2)}</pre>
             </div>
           </div>
-        :null}
-        {showEventLog?
-        <div className={`eventViewContainer ${logPosition === "L" ? "onLeft" : "onRight"}`}>
-          <button 
-          className="closeButton" 
-          onClick={closeModal}>Close
-          </button>
-          <div className="previewWrapper">
-            <pre className="codePreview">
-              {JSON.stringify(currentEvent, null, 2)}
-            </pre>
-          </div>
-        </div> : null
-        }
+        ) : null}
       </div>
     </div>
   );
